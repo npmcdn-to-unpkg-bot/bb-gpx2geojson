247ad33132b6d25d2ed10b59866b10e00fbc6e95
Filipe Abrahao <me@luizfilipe.com>
2016-08-09 20:08:11 +0100

    database config set and port changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const app = require('./app');
-const port = app.get('port');
+const configPort = app.get('port');
+
+const port = process.env.PORT || configPort;
+
 const server = app.listen(port);
 
 server.on('listening', () =>
